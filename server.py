bb851987d80c9c4b2caf24c6213948d1b8922f3a
Kelvin <kelvinjoliano18@gmail.com>
2025-06-22 16:32:58 +0700

    ah mbuh ah

--- server.py
+++ server.py
@@ -11,7 +11,7 @@ app = FastAPI()
 # ✅ Izinkan akses dari domain WordPress
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://petroenergisafety.com"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
